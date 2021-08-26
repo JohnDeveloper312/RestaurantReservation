14ca8ad5552a35c4b6bac4258ccfcff644f3d1e9
John Orfanakos <yianni312@gmail.com>
2021-08-26 11:09:35 -0400

    added validation for reservation

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -4,14 +4,7 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 /**
  * List handler for reservation resources
  */
-const VALID_PROPERTIES = [
-  "first_name",
-  "last_name",
-  "mobile_number",
-  "reservation_date",
-  "reservation_time",
-  "people",
-];
+
 
 async function list(req, res) {
   const { date } = req.query;
@@ -29,22 +22,39 @@ async function create(req, res, next) {
   res.status(201).json({ data: newReservation });
 }
 
-function hasOnlyValidProperties(req, res, next) {
-  const { data = {} } = req.body;
-  res.locals.reservation = req.body.data;
-  
-  const invalidFields = Object.keys(data).filter((field) => {
-    !VALID_PROPERTIES.includes(field);
-  });
-  if (invalidFields.length)
-    return next({
-      status: 400,
-      message: `Invalid field(s): ${invalidFields.join(",")}`,
-    });
+function resCheck(req, res, next) {
+  const {
+    data: {
+      first_name,
+      last_name,
+      mobile_number,
+      reservation_date,
+      reservation_time,
+      people,
+    },
+  } = req.body;
+  if (!first_name || first_name == "")
+    return next({ status: 400, message: `First name is required` });
+  if (!last_name || last_name == "")
+    return next({ status: 400, message: `Last name is required` });
+  if (!mobile_number || (mobile_number == "" && isNaN(mobile_number)))
+    return next({ status: 400, message: `Mobile number is required` });
+  if (!reservation_date)
+    return next({ status: 400, message: `Please select a reservation date` });
+  if (!reservation_time)
+    return next({ status: 400, message: `Please select a reservation time` });
+  res.locals.reservation = {
+    first_name,
+    last_name,
+    mobile_number,
+    reservation_date,
+    reservation_time,
+    people,
+  };
   next();
 }
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: [hasOnlyValidProperties, asyncErrorBoundary(create)],
+  create: [resCheck, asyncErrorBoundary(create)],
 };
